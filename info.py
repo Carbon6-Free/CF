f70e2bf86d161b9885649c96e482e26377917cf8
Hwayoung Kim <147595460+keemhhyy@users.noreply.github.com>
2024-09-18 16:48:40 +0900

    Update info.py

--- info.py
+++ info.py
@@ -48,31 +48,31 @@ def carbon_info(carbon_g):
     if carbon_g < 1:
         pass
     elif carbon_g < 10:
-        st.image('./assets/light4.png', width=200)
+        st.image('./pic/light4.png', width=200)
         st.write("링크 10회 방문시 형광등 40분을 사용한 탄소가 발생됩니다.")
     elif carbon_g < 20:
-        st.image('./assets/light8.png', width=200)
+        st.image('./pic/light8.png', width=200)
         st.write("링크 10회 방문시 형광등 80분을 사용한 탄소가 발생됩니다.")
     elif carbon_g < 30:
-        st.image('./assets/tv3.png', width=200)
+        st.image('./pic/tv3.png', width=200)
         st.write("링크 10회 방문시 TV를 30분동안 사용한 탄소가 발생됩니다.")
     elif carbon_g < 40:
-        st.image('./assets/phone.png', width=200)
+        st.image('./pic/phone.png', width=200)
         st.write("링크 10회 방문시 스마트폰을 2시간동안 사용한 탄소가 발생됩니다.")
     elif carbon_g < 50:
-        st.image('./assets/aircon.png', width=200)
+        st.image('./pic/aircon.png', width=200)
         st.write("링크 10회 방문시 에어컨을 15분동안 사용한 탄소가 발생됩니다.")
     elif carbon_g < 60:
-        st.image('./assets/com4.png', width=200)
+        st.image('./pic/com4.png', width=200)
         st.write("링크 10회 방문시 컴퓨터 4시간동안 사용한 탄소가 발생합니다.")
     elif carbon_g < 70:
-        st.image('./assets/wash2.png', width=200)
+        st.image('./pic/wash2.png', width=200)
         st.write("링크 10회 방문시 세탁기를 20분동안 사용한 탄소가 발생합니다.")
     elif carbon_g < 80:
-        st.image('./assets/oven3.png', width=200)
+        st.image('./pic/oven3.png', width=200)
         st.write("링크 10회 방문시 전자레인지를 30분동안 사용한 탄소가 발생합니다.")
     elif carbon_g > 100:
-        st.image('./assets/car.png', width=200)
+        st.image('./pic/car.png', width=200)
         st.write(f"링크 10회 방문시 발생되는 탄소는 약 {int(carbon_g)}g 입니다. 차로 2.5km를 이동했을때 발생되는 탄소는 560g 입니다.")
 
 
