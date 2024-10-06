93ce44d7b74727f975c08794dd0575e1c9cfc292
Hwayoung Kim <147595460+keemhhyy@users.noreply.github.com>
2024-10-06 18:06:56 +0900

    Update main.py

--- main.py
+++ main.py
@@ -10,40 +10,19 @@ from style import set_style
 from info import display_random_info, carbon_info
 import json
 from lib.graph import *
+from dotenv import load_dotenv
 
 with open('carbonfree.json') as f:
     data = json.load(f)
+
+load_dotenv()
 
 API_KEY=os.getenv("firebasekey")
 firebase=firebase.FirebaseApplication(API_KEY, None)
 with open("./cutoff.txt", 'rb') as lf:
     cutoff = pickle.load(lf)
 
-# CPU 도넛 차트
-cpu_fig = go.Figure(go.Pie(
-    values=[20, 80],
-    hole=0.6,
-    marker=dict(colors=['#FF6361', 'rgba(0,0,0,0)'])  # 나머지 부분을 투명하게 설정
-))
-cpu_fig.update_layout(
-    showlegend=False,
-    annotations=[dict(text='CPU', x=0.5, y=0.5, font_size=20, showarrow=False)],
-    width=300, 
-    height=300
-)
 
-# GPU 도넛 차트
-gpu_fig = go.Figure(go.Pie(
-    values=[40, 60],
-    hole=0.6,
-    marker=dict(colors=['#FFA600', 'rgba(0,0,0,0)'])  # 나머지 부분을 투명하게 설정
-))
-gpu_fig.update_layout(
-    showlegend=False,
-    annotations=[dict(text='GPU', x=0.5, y=0.5, font_size=20, showarrow=False)],
-    width=300, 
-    height=300
-)
 
 # Apply the style settings
 set_style()
@@ -102,15 +81,7 @@ if st.button("View results"):
 
     st.write("링크를 한번 방문할떄마다 " , carbon_g, "g의 탄소가 발생합니다.")
 
-    # 도넛차트 추가
-    # Streamlit에 양옆 배치
-    col1, col2 = st.columns(2)
-
-    with col1:
-        st.plotly_chart(cpu_fig)
-
-    with col2:
-        st.plotly_chart(gpu_fig)
+    
 
     carbon_info(carbon_g * 10)
     st.image(image, width=200)
